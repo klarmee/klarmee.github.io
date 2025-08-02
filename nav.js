f29754c44ded9f6a085a75d49a778c936a8d945f
klarmee <126979369+klarmee@users.noreply.github.com>
2025-08-02 05:34:29 +0000

    s

--- nav.js
+++ nav.js
@@ -1,4 +1,10 @@
 document.addEventListener('keydown', (event) => {
+  // Check if any modifier keys are being pressed.
+  // If so, exit the function to avoid interfering with shortcuts.
+  if (event.ctrlKey || event.shiftKey || event.altKey || event.metaKey) {
+    return;
+  }
+
   // Check if the key pressed is the left arrow
   if (event.key === 'ArrowLeft') {
     // Find the link with the text "Previous"
